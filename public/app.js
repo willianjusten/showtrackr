43181bda2bbb736201e55f32395070ffcb1eeb17
Willian Justen <willianjustenqui@gmail.com>
2014-07-01 01:44:47 -0300

    Add Routes and Templates

--- public/app.js
+++ public/app.js
@@ -1,5 +1,29 @@
-angular.module('myApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-	.config(function(){
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
 
-	});
-
+		$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'MainCtrl'
+		})
+		.when('/shows/:id', {
+			templateUrl: 'views/detail.html',
+			controller: 'DetailCtrl'
+		})
+		.when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'LoginCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 'views/signup.html',
+			controller: 'SignupCtrl'
+		})
+		.when('/add', {
+			templateUrl: 'views/add.html',
+			controller: 'AddCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	}]);
