4c3142e76282fdc95a6c6724e89f54796be1dc6e
Willian Justen <willian@maxisite.net>
2014-07-01 17:17:20 -0300

    Add Mongoose and Bcrypt

--- server.js
+++ server.js
@@ -1,10 +1,71 @@
+// requiring modules
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 var app = express();
+
+// MongoDB Schema
+var showSchema = new mongoose.Schema({
+	_id: Number,
+	name: String,
+	airsDayOfWeek: String,
+	airsTime: String,
+	firstAired: Date,
+	genre: [String],
+	network: String,
+	overview: String,
+	rating: Number,
+	ratingCount: Number,
+	status: String,
+	poster: String,
+	subscribers: [{
+		type: mongoose.Schema.Types.ObjectId, ref: 'User'
+	}],
+	episodes: [{
+		season: Number,
+		episodeNumber: Number,
+		episodeName: String,
+		firstAired: Date,
+		overview: String
+	}]
+});
+
+var userSchema = new mongoose.Schema({
+	email: { type: String, unique: true },
+	password: String
+});
+
+userSchema.pre('save', function(next) {
+	var user = this;
+	if (!user.isModified('password')) return next();
+	bcrypt.genSalt(10, function(err, salt) {
+		if (err) return next(err);
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+ 
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+// Define Models
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+
+// Connect 
+mongoose.connect('localhost');
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
@@ -14,5 +75,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
