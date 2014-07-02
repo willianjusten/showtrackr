d490e33c49a76fdfe950db576064c1fb5e036051
Willian Justen <willianjustenqui@gmail.com>
2014-07-02 17:57:06 -0300

    New Add and Detail pages

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+var _ = require('lodash');
 
 var app = express();
 
@@ -52,7 +56,7 @@ userSchema.pre('save', function(next) {
 		});
 	});
 });
- 
+
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
 	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
 		if (err) return cb(err);
@@ -65,7 +69,7 @@ var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
 // Connect 
-mongoose.connect('localhost');
+mongoose.connect('mongodb://localhost/showtrackr');
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
@@ -74,6 +78,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// GET CONTENTS
 app.get('/api/shows', function(req, res, next) {
 	var query = Show.find();
 	if (req.query.genre) {
@@ -97,12 +102,95 @@ app.get('/api/shows/:id', function(req, res, next) {
 });
 
 app.get('*', function(req, res) {
-  res.redirect('/#' + req.originalUrl);
+	res.redirect('/#' + req.originalUrl);
+});
+
+// SAVE SHOWS
+app.post('/api/shows', function(req, res, next) {
+	var apiKey = '9EF1D1E7D28FDA0B';
+	var parser = xml2js.Parser({
+		explicitArray: false,
+		normalizeTags: true
+	});
+
+	// slugfying the names
+	var seriesName = req.body.showName
+	.toLowerCase()
+	.replace(/ /g, '_')
+	.replace(/[^\w-]+/g, '');
+
+	async.waterfall([
+		function(callback) {
+			request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
+				if (error) return next(error);
+				parser.parseString(body, function(err, result) {
+					if (!result.data.series) {
+						return res.send(404, { message: req.body.showName + ' was not found.' });
+					}
+					var seriesId = result.data.series.seriesid || result.data.series[0].seriesid;
+					callback(err, seriesId);
+				});
+			});
+		},
+		function(seriesId, callback) {
+			request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(error, response, body) {
+				if (error) return next(error);
+				parser.parseString(body, function(err, result) {
+					var series = result.data.series;
+					var episodes = result.data.episode;
+					var show = new Show({
+						_id: series.id,
+						name: series.seriesname,
+						airsDayOfWeek: series.airs_dayofweek,
+						airsTime: series.airs_time,
+						firstAired: series.firstaired,
+						genre: series.genre.split('|').filter(Boolean),
+						network: series.network,
+						overview: series.overview,
+						rating: series.rating,
+						ratingCount: series.ratingcount,
+						runtime: series.runtime,
+						status: series.status,
+						poster: series.poster,
+						episodes: []
+					});
+					_.each(episodes, function(episode) {
+						show.episodes.push({
+							season: episode.seasonnumber,
+							episodeNumber: episode.episodenumber,
+							episodeName: episode.episodename,
+							firstAired: episode.firstaired,
+							overview: episode.overview
+						});
+					});
+					callback(err, show);
+				});
+			});
+		},
+		function(show, callback) {
+			var url = 'http://thetvdb.com/banners/' + show.poster;
+			request({ url: url, encoding: null }, function(error, response, body) {
+				show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+				callback(error, show);
+			});
+		}
+		], function(err, show) {
+			if (err) return next(err);
+			show.save(function(err) {
+				if (err) {
+					if (err.code == 11000) {
+						return res.send(409, { message: show.name + ' already exists.' });
+					}
+					return next(err);
+				}
+				res.send(200);
+			});
+		});
 });
 
 app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.send(500, { message: err.message });
+	console.error(err.stack);
+	res.send(500, { message: err.message });
 });
 
 app.listen(app.get('port'), function() {
