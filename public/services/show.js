60a0e567da18d11d87aa15c9a5b667ebd903b179
Willian Justen <willianjustenqui@gmail.com>
2014-07-03 15:31:24 -0300

    Login and Signup Frontend

--- public/services/show.js
+++ public/services/show.js
@@ -1,4 +1,4 @@
 angular.module('MyApp')
-	.factory('Show', ['$resource', function ($resource) {
-		return $resource('api/shows:_id');
-	}]);
+  .factory('Show', ['$resource', function($resource) {
+    return $resource('/api/shows/:_id');
+  }]);
