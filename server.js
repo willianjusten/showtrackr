f9041ba54b8dd7e803e1094bb092bab500bff7a4
Willian Justen <willianjustenqui@gmail.com>
2014-07-07 11:23:39 -0300

    Add subscription

--- server.js
+++ server.js
@@ -134,6 +134,30 @@ app.post('/api/signup', function(req, res, next) {
 	});
 });
 
+// SUBSCRIPTIONS
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+	Show.findById(req.body.showId, function(err, show) {
+		if (err) return next(err);
+		show.subscribers.push(req.user.id);
+		show.save(function(err) {
+			if (err) return next(err);
+			res.send(200);
+		});
+	});
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+	Show.findById(req.body.showId, function(err, show) {
+		if (err) return next(err);
+		var index = show.subscribers.indexOf(req.user.id);
+		show.subscribers.splice(index, 1);
+		show.save(function(err) {
+			if (err) return next(err);
+			res.send(200);
+		});
+	});
+});
+
 // GET CONTENTS
 app.get('/api/shows', function(req, res, next) {
 	var query = Show.find();
